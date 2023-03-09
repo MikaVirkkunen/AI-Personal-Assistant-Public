ac8a0ff6c396e6758c3bda69ec5d1eae18622e54
MikaVirkkunen <Mika.virkkunen@outlook.com>
2023-03-09 20:50:41 +0200

    toimiva notebookki

--- assistant.py
+++ assistant.py
@@ -1,88 +1,79 @@
-from dotenv import load_dotenv
-from datetime import datetime
 import os
-import time
+import azure.cognitiveservices.speech as speechsdk
+import openai
+import re
+import requests
+import sys
+from num2words import num2words
+import pandas as pd
+import numpy as np
+import azure.cognitiveservices.speech as speechsdk
 
-# Import namespaces
-import azure.cognitiveservices.speech as speech_sdk
+openai.api_type = "azure"
+openai.api_key = os.getenv('OPENAI_API_KEY') 
+openai.api_base = os.getenv("OPENAI_API_ENDPOINT")
+openai.api_version = "2022-12-01"
 
-from playsound import playsound
+COMPLETIONS_MODEL = "text-davinci-003"
 
-def main():
-    try:
-        global speech_config
+def recognize_from_microphone():
+    # This example requires environment variables named "SPEECH_KEY" and "SPEECH_REGION"
+    speech_config = speechsdk.SpeechConfig(subscription=os.environ.get('COG_SERVICE_KEY'), region=os.environ.get('COG_SERVICE_REGION'))
+    speech_config.speech_recognition_language="en-US"
 
-        # Get Configuration Settings
-        load_dotenv()
-        cog_key = os.getenv('COG_SERVICE_KEY')
-        cog_region = os.getenv('COG_SERVICE_REGION')
+    audio_config = speechsdk.audio.AudioConfig(use_default_microphone=True)
+    speech_recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config, audio_config=audio_config)
 
-        # Configure speech service
-        speech_config = speech_sdk.SpeechConfig(cog_key, cog_region)
-        print('Ready to use speech service in:', speech_config.region)
+    print("Speak into your microphone.")
+    speech_recognition_result = speech_recognizer.recognize_once_async().get()
 
-        # Get spoken input
-        command = TranscribeCommand()
-        if command.lower() == 'what time is it?':
-            TellTime()
+    if speech_recognition_result.reason == speechsdk.ResultReason.RecognizedSpeech:
+        print("Recognized: {}".format(speech_recognition_result.text))
+        foo = "{}".format(speech_recognition_result.text)
+    elif speech_recognition_result.reason == speechsdk.ResultReason.NoMatch:
+        print("No speech could be recognized: {}".format(speech_recognition_result.no_match_details))
+    elif speech_recognition_result.reason == speechsdk.ResultReason.Canceled:
+        cancellation_details = speech_recognition_result.cancellation_details
+        print("Speech Recognition canceled: {}".format(cancellation_details.reason))
+        if cancellation_details.reason == speechsdk.CancellationReason.Error:
+            print("Error details: {}".format(cancellation_details.error_details))
+            print("Did you set the speech resource key and region values?")
+    return foo
+foo = recognize_from_microphone()
 
-    except Exception as ex:
-        print(ex)
+prompt = foo
+output = openai.Completion.create(
+    prompt=prompt,
+    temperature=0,
+    max_tokens=1000,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0,
+    engine=COMPLETIONS_MODEL
+)["choices"][0]["text"].strip(" \n")
 
-def TranscribeCommand():
-    command = ''
+# This example requires environment variables named "SPEECH_KEY" and "SPEECH_REGION"
+speech_config = speechsdk.SpeechConfig(subscription=os.environ.get('COG_SERVICE_KEY'), region=os.environ.get('COG_SERVICE_REGION'))
+audio_config = speechsdk.audio.AudioOutputConfig(use_default_speaker=True)
 
-    # Configure speech recognition
-    audio_config = speech_sdk.AudioConfig(use_default_microphone=True)
-    speech_recognizer = speech_sdk.SpeechRecognizer(speech_config, audio_config)
-    print('Speak now...')
+# The language of the voice that speaks.
+speech_config.speech_synthesis_voice_name='en-US-JaneNeural'
 
-    # Process speech input
-    speech = speech_recognizer.recognize_once_async().get()
-    if speech.reason == speech_sdk.ResultReason.RecognizedSpeech:
-        command = speech.text
-        print(command)
-    else:
-        print(speech.reason)
-        if speech.reason == speech_sdk.ResultReason.Canceled:
-            cancellation = speech.cancellation_details
-            print(cancellation.reason)
-            print(cancellation.error_details)
+speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config, audio_config=audio_config)
 
-    # Return the command
-    return command
+# Get text from the console and synthesize to the default speaker.
+# print("Enter some text that you want to speak >")
+# text = input()
+text = output
 
+speech_synthesis_result = speech_synthesizer.speak_text_async(text).get()
 
-def TellTime():
-    now = datetime.now()
-    response_text = 'The time is {}:{:02d}'.format(now.hour,now.minute)
-
-
-    # Configure speech synthesis
-    speech_config.speech_synthesis_voice_name = "en-GB-RyanNeural"
-    speech_synthesizer = speech_sdk.SpeechSynthesizer(speech_config)
-
-    # Synthesize spoken output
-    speak = speech_synthesizer.speak_text_async(response_text).get()
-    if speak.reason != speech_sdk.ResultReason.SynthesizingAudioCompleted:
-        print(speak.reason)
-
-    # Print the response
-    print(response_text)
-
-
-if __name__ == "__main__":
-
-# Set the start time
- start_time = time.time()
-
-main()  
-
-# Calculate the elapsed time  
-elapsed_time = time.time() - start_time  
-
-# Check if 10 seconds have passed  
-if elapsed_time >= 10:  
-    print("Program ended after 10 seconds.")  
-else:  
-    print("Program ended before 10 seconds.")
+if speech_synthesis_result.reason == speechsdk.ResultReason.SynthesizingAudioCompleted:
+    print("Speech synthesized for text [{}]".format(text))
+elif speech_synthesis_result.reason == speechsdk.ResultReason.Canceled:
+    cancellation_details = speech_synthesis_result.cancellation_details
+    print("Speech synthesis canceled: {}".format(cancellation_details.reason))
+    if cancellation_details.reason == speechsdk.CancellationReason.Error:
+        if cancellation_details.error_details:
+            print("Error details: {}".format(cancellation_details.error_details))
+            print("Did you set the speech resource key and region values?")
